f28e3bcdf2a2bb5b8d29bc41a0a8886a95d7c705
timikid <timikit@naver.com>
2020-12-03 16:03:01 +0900

    2

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "linkedlist.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 struct Book{
 	int number;
@@ -8,24 +8,27 @@ struct Book{
 }; 
 
 int main(int argc, char *argv[]) {
-	struct Book *p;
+	int i;
+	struct Book **bookshelf;
 	
-	p = (struct Book*)malloc(2*sizeof(struct Book));
+	bookshelf = (struct Book**)malloc(3*sizeof(struct Book*));  //1차원 포인터변수  
 	
-	if(p==NULL){
-		printf("memory allocation error\n");
-		return -1;
-	}
+	for(i=0;i<3;i++)
+	   bookshelf[i]=malloc(10*sizeof(struct Book));
 	
-	p->number =1;  
-	strcpy(p->title,"Cprogramming");
-	
-	(p+1)->number =2;
-	strcpy((p+1)->title, "Electronics");
-	
-	printf("%s %s\n", p->title, (p+1)->title);
-	free(p);
-  
+    bookshelf[1][3].number =5;
+    strcpy(bookshelf[1][3].title, "C++programming");
+    
+    (bookshelf[2]+4)->number =3;   //2,4에 접근  
+    strcpy((bookshelf[2]+4)->title, "Cprogramming Theory");
+    
+    printf("Book(1,3) :%i, %s\n", (bookshelf[1]+3)->number,(bookshelf[1]+3)->title );
+    printf("Book(2,4) :%i, %s\n", bookshelf[2][4].number,bookshelf[2][4].title );
+    
+	for(i=0;i<3;i++)
+       free(bookshelf[i]);
+	free(bookshelf); 
+     
 	
 	return 0;
 }
